c5e0098165f9d478e9fd6dd603cd15e9bbebb832
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2021-11-29 17:14:24 -0600

    Quotes array

---
+++ quoteArr.js
@@ -0,0 +1,286 @@
+const quotes=[
+	{
+    "quote": "“I saw this movie about a bus that had to speed around a city, keeping its speed over fifty, and if its speed dropped, it would explode! I think it was called, 'The Bus That Couldn’t Slow Down'.",
+    "author": "Homer Simpson"
+    }, {
+    "quote": "You tried your best and you failed miserably. The lesson is: never try.",
+    "author": "Homer Simpson"
+    }, {
+    "quote": "“I am so smart! I am so smart! S-M-R-T … I mean S-M-A-R-T!.”",
+    "author": "Homer Simpson"
+    }, {
+    "quote": "I used to be with it, but then they changed what ‘it’ was, and now what I’m with isn’t it. And what’s ‘it’ seems weird and scary to me.",
+    "author": "Grampa Simpson"
+    }, {
+    "quote": "And I, For One, Welcome Our New Insect Overlords.",
+    "author": "Kent Brockman"
+    }, {
+    "quote": "Loneliness and cheeseburgers are a dangerous mix.",
+    "author": "Comic Book Guy"
+	}, {
+	"quote": "Family, religion, friendship. These are the three demons you must slay if you wish to succeed in busniess.",
+	"author": "Mr. Burns"
+	}, {
+	"quote": "Ironic, isn't it Smithers? This anonymous clan of slack-jawed troglodytes has cost me the election, and yet if I were to have them killed, I would be the one to go to jail. That's democracy for you!",
+	"author": "Mr. Burns"
+	}, {
+	"quote": "I've argued in front of every judge in this state. Often as a lawyer!",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": "Mr. Simpson, this is the most blatant case of fraudulent advertising since my suit against the film 'The NeverEnding Story'.",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": "I guess one person can make a difference. But most of the time, they probably shouldn’t.",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "Shut up, brain! I've got friends now. I don't need you anymore.",
+	"author": "Lisa Simpson"
+	}, {
+	"quote": " I don't get. Straight A's, perfect attendance, bathroom timer. I should be the most popular girl in school.",
+	"author": "Lisa Simpson"
+	}, {
+	"quote": "Ah, there's nothing more exciting than science. You get all the fun of sitting still, being quiet, writing down numbers, paying attention... Science has it all.",
+	"author": "Principle Seymour Skinner"
+	}, {
+	"quote": "Oh no, I said 'steamed hams'. That's what I call hamburgers.",
+	"author": "Principle Seymour Skinner"
+	}, {
+	"quote": "If he’s so smart, how come he’s dead?",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Fame was like a drug, but what was even more like a drug were the drugs.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "I’ve learned that life is one crushing defeat after another until you just wish Flanders was dead.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Mr. Simpson, I don't use the word 'hero' lightly, but you are the greatest hero in American history. ",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": "Mr. Simpson, don't you worry. I saw an episode of Matlock in a bar last night. The sound was down, but I think I got the gist of it. ",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": "Mr. Simpson, I was just going through your garbage and I couldn't help overhearing that you need a babysitter! Of course, being a highly-skilled attorney, my fee is $175 an hour.",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": " A poem, by Hans Moleman: I think that I shall never see, my cataracts are blinding me.",
+	"author": "Hans Moleman"
+	}, {
+	"quote": "A woman is a lot like a refrigerator: 6 feet tall, 300 pounds… it makes ice.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "I think the saddest day of my life was when I realized I could beat my dad at most things, and Bart experienced that at the age of four.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "I was working on a flat tax proposal and I accidentally proved there's no God.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Son, being popular is the most important thing in the whole world!",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "I'm a white male, age 18 to 49. Everyone listens to me, no matter how dumb my suggestions are.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Step aside, everyone! Sensitive love letters are my specialty. 'Dear Baby, Welcome to Dumpsville. Population: you.'",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Mom, Dad, just so you don’t hear any wild rumors, I’m being indicted for fraud in Australia.",
+	"author": "Bart Simpson"
+	},{
+	"quote": "There’s a 4:30 in the morning now?",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "I'm Bart Simpson, who the hell are you?",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "Now what can I ding dong diddly do for you?",
+	"author": "Ned Flanders"
+	}, {
+	"quote": "Hi diddly ho neighborinos!",
+	"author": "Ned Flanders"
+	}, {
+	"quote": "Iron helps us play!",
+	"author": "Todd Flanders"
+	}, {
+	"quote": "I'm not thinking straight, why did I have that wine cooler last month?",
+	"author": "Ned Flanders"
+	}, {
+	"quote": "Bless the grocer for this wonderful meat, the middleman who jacked up the price, and let’s not forget the humane but determined boys at the slaughterhouse.",
+	"author": "Ned Flanders"
+	}, {
+	"quote": "Now, where were we? Oh yeah, the important thing was I had an onion on my belt, which was the style at the time. They didn't have white onions because of the war. The only thing you could get was those big yellow ones.",
+	"author": "Grampa Simpson"
+	}, {
+	"quote": "Very well then, instead of fleeing this town, I'll stay here and grow fat off kickbacks and slush funds.",
+	"author": "Mayor Quimby"
+	}, {
+	"quote": "Are these morons getting dumber or just louder?",
+	"author": "Mayor Quimby"
+	}, {
+	"quote": "Our guilty conscience may force you to vote Democratic, but deep down inside you secretly long for a cold-hearted Republican to lower taxes, brutalize criminals, and rule you like a king. That's why I did this: to protect you from yourselves!",
+	"author": "Sideshow Bob"
+	}, {
+	"quote": " A great man once observed: '90% of success is showing up on time.' Sorry I'm four hours late...",
+	"author": "Krusty the Clown"
+	}, {
+	"quote": "These tests will have no effect on your grades. They merely determine your future social status and financial success. If any.",
+	"author": "Edna Krabappel"
+	}, {
+	"quote": "HA!",
+	"author": "Edna Krabappel"
+	}, {
+	"quote": "And When The Doctor Said I Didn't Have Worms Anymore, That Was The Happiest Day Of My Life.",
+	"author": "Ralph Wiggum"
+	}, {
+	"quote": "I'm in danger!",
+	"author": "Ralph Wiggum"
+	}, {
+	"quote": "Me fail English...that's unpossible!",
+	"author": "Ralph Wiggum"
+	}, {
+	"quote": "Having a weapon at school has really made things awesome.",
+	"author": "Millhouse Van Houten"
+	}, {
+	"quote": "His jiggling is almost hypnotic! Yes, it's like a lava lamp.",
+	"author": "Dr. Hibbert"
+	}, {
+	"quote": "I hate every ape I see from From chimpan-a to chimpan-z.",
+	"author": "Troy McClure"
+	}, {
+	"quote": "Hi, I'm Troy Mcclure. You Might Remember Me From Such Public Service Videos As Designated Drivers: The Lifesaving Nerds And Phony Tornado Alarms Reduce Readiness. I'm Here Today To Give You The Skinny On Shoplifting, Thereby Completing My Plea Bargain With The Good People At Foot Locker Of Beverly Hills.",
+	"author": "Troy McClure"
+	}, {
+	"quote": "Eat my Shorts!",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "Don't have a cow!",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "D'oh!",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Why you little!",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "*Groans in disapproval*",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "Did you know that disco record sales were up 400% for the year ending 1976? If these trends continue... eyyyy!",
+	"author": "Disco Stu"
+	}, {
+	"quote": "Moon pie... what a time to be alive.",
+	"author": "Jasper Beardsley"
+	}, {
+	"quote": "Love is the flower you've got to let grow.",
+	"author": "John Lennon"
+	}, {
+	"quote": "Hello! I'm Troy McClure. You may remember me from such self-help videos as Smoke Yourself Thin and Get Confident, Stupid.",
+	"author": "Troy McClure"
+	}, {
+	"quote": "As I said to Dolores Montenegro in Calling All Quakers, 'Have it your way, baby.'",
+	"author": "Troy McClure"
+	}, {
+	"quote": " If you ask me they're all winners. We'll be cutting our first 40 contestants right after this.",
+	"author": "Troy McClure"
+	}, {
+	"quote": "I wish God were alive to see this.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "What's the point of going out? We're just gonna wind up back here anyway..",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Okay, I'm not going to give up. Solitude never hurt anyone. Emily Dickinson lived alone, and she wrote some of the most beautiful poetry the world has ever known... then went crazy as a loon.",
+	"author": "Lisa Simpson"
+	}, {
+	"quote": "Mom, romance is dead. It was acquired by Hallmark and Disney in a hostile takeover, homogenized, and sold off piece by piece.",
+	"author": "Lisa Simpson"
+	}, {
+	"quote": "Between your genius and my nothing we make a great team, come on give me a hug!",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Today he's drinking people's blood. Tomorrow he could be smoking.",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "If you raised three children who can knock out and hog tie a perfect stranger you must be doing something right.",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "Want me to zinc your sniffer?",
+	"author": "Ned FLanders"
+	}, {
+	"quote": "You have 24 hours to give us our money. And to show you we're serious ... you have 12 hours.",
+	"author": "Fat Tony"
+	}, {
+	"quote": "You're turning me into a criminal when all I want to be is a petty thug!",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "I didn't think it was physically possible, but this both sucks and blows!",
+	"author": "Bart Simpson"
+	}, {
+	"quote": "Talk to the audience? Oh, this part is always death.",
+	"author": "Krusty the Clown"
+	}, {
+	"quote": "There's no justice like angry-mob justice.",
+	"author": "Principle Skinner"
+	}, {
+	"quote": "You know, the courts may not be working any more, but as long as everyone is videotaping everyone else, justice will be done.",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "I just realized we never had a wedding for the cat and the dog. They've been living in sin.",
+	"author": "Marge Simpson"
+	}, {
+	"quote": "America's health care system is second only to Japan ... Canada, Sweden, Great Britain ... well, all of Europe. But you can thank your lucky stars we don't live in Paraguay.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "I AM THE LIZARD QUEEN!",
+	"author": "Lisa Simpson"
+	}, {
+	"quote": "The deed is done. 24 years of trying to kill a ten-year-old child have finally paid off.",
+	"author": "Sideshow Bob"
+	}, {
+	"quote": "They didn't have any aspirin, so I got you some cigarettes.",
+	"author": "Homer Simpson"
+	}, {
+	"quote": "Just miles from your doorstep, hundreds of men are given weapons and trained to kill. The government calls it the Army, but a more alarmist name would be ... the Killbot Factory.",
+	"author": "Kent Brockman"
+	}, {
+	"quote": "If anyone wants me, I'll be in my room.",
+	"author": "Lisa Simpson`"
+	}, {
+	"quote": "Uh, no, they're saying, 'Boo-urns! Boo-urns!",
+	"author": "Waylon Smithers"
+	}, {
+	"quote": "There goes the last lingering thread of my heterosexuality.",
+	"author": "Patty Bouvier"
+	}, {
+	"quote": "Ned, have you considered any of the other major religions? They're all pretty much the same.",
+	"author": "Reverend Lovejoy"
+	}, {
+	"quote": "Bake 'em away, toys!",
+	"author": "Officer Wiggum"
+	}, {
+	"quote": "I’m better than dirt. Well, most kinds of dirt.",
+	"author": "Moe Szyslak"
+	}, {
+	"quote": "Everything's coming up Milhouse!",
+	"author": "Millhouse Van Houten"
+	}, {
+	"quote": "He's had it in for me ever since I kinda ran over his dog. Well, replace the word 'kinda' with the word 'repeatedly,' and the word 'dog' with 'son.",
+	"author": "Lionel Hutz"
+	}, {
+	"quote": "Don't kid yourself, Jimmy. If a cow ever got the chance, he'd eat you and everyone you care about!",
+	"author": "Troy McClure"
+	}, {
+	"quote": "Oh, I have had it, I have had it with this school, Skinner! The low test scores, class after class of ugly, ugly children!",
+	"author": "Superintendent Chalmers"
+	}, {
+	"quote": "Tell him it's the 'worst prognosis ever!' Hee hee hee.",
+	"author": "Dr. Hibbert"
+	}, {
+	"quote": "Have the Rolling Stones killed.",
+	"author": "Mr. Burns"
+	}, {
+	"quote": "Daddy says dice are wicked!",
+	"author": "Todd Flanders"
+	}
+]
+export default quotes
