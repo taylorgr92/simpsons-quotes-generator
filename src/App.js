43d57ad02ca3047de10bb143ad7d6dad890899f8
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-03-26 17:57:54 -0500

    Toggle Menu skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import QuoteBox from "./quote-box";
 import React from "react";
 import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import quotes from "./quoteArr";
 
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
 class App extends React.Component {
   constructor() {
     super();
---
+++ src/ToggleMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const ToggleMenu = () => {
+    return(
+x+y    );
+};
+
+
+export default ToggleMenu;
