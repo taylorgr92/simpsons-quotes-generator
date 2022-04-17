af59f98e2ab7aaee0e591c6c9870b636e0ccd33c
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-04-17 13:10:29 -0500

    Contact card columns

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import {Card} from "react-bootstrap";
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
 
 const ContactMe = () => {
-    return(
-    
-       <div>
-<Card className= "contact-card">
-
-</Card>
-       </div>
-    )
-}
+  return (
+    <div>
+      <Card className="contact-card">
+        <Card.Title className="mx-auto"> Taylor Franklin </Card.Title>{" "}
+        <Row>
+          <Col>1 </Col> <Col>2 </Col> <Col>3 </Col>{" "}
+        </Row>{" "}
+      </Card>{" "}
+    </div>
+  );
+};
 
 export default ContactMe;
