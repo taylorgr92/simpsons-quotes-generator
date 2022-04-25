da06f2ad3bd108b92d25cf5324ac617258a85256
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-04-25 17:30:30 -0500

    remove react transitions

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import ToggleMenu from "./components/ToggleMenu";
 import MyFooter from "./components/MyFooter";
 import Home from "./components/HomePage";
 import ContactMe from "./components/ContactMe";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./styles.css";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      <ToggleMenu/>
-      <CSSTransition
-      timeout={500}
-      classNames="fade">
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/home" element={ <Home/> } />
-        <Route path="contact" element={ <ContactMe/> } />
-      </Routes>
-      </CSSTransition>
-      <MyFooter/>
+        <ToggleMenu />
+        <Routes>
+\
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+
+        <MyFooter />
       </div>
     );
   }
