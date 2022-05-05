a6a46f2b620cd996347daef84e8a965ad34e3c87
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-05-04 21:22:49 -0500

    Contact card margin bottom

--- src/components/QuoteContainer.js
+++ src/components/QuoteContainer.js
@@ -1,36 +1,27 @@
 import React from "react";
-import {motion} from 'framer-motion/dist/framer-motion'
-
 
 class QuoteContainer extends React.Component {
   render() {
-
     return (
-
- <div>
-     
-
+      <div>
         <div className="quote-container">
-
           <p className="quote-text">{this.props.quote}</p>
 
-            
-            <img src={this.props.image} alt="Simpsons character" className= "simspson-image"/>
+          <img
+            src={this.props.image}
+            alt="Simpsons character"
+            className="simspson-image"
+          />
 
+          <p class="author-name">{this.props.author}</p>
 
-           <p class="author-name">{this.props.author}</p>
-
-<div >
-           <button className="quote-button" onClick={this.props.handleClick}>
-            Next Quote!
-          </button>
+          <div>
+            <button className="quote-button" onClick={this.props.handleClick}>
+              Next Quote!
+            </button>
           </div>
-      
-    </div>
-    </div>
-
-
-
+        </div>
+      </div>
     );
   }
 }
