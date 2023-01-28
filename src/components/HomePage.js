81859cb27a7e16617fa413192f5e8372503dbf81
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2023-01-28 13:19:38 -0600

    refactored Home component into functional component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,42 +2,33 @@ import React from "react";
 import QuoteContainer from "./QuoteContainer";
 import quotes from "./quote-arr.js";
 
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      quote: quotes[0].quote,
-      author: quotes[0].author,
-      image: quotes[0].image,
-    };
-  }
+const Home = () => {
+  const [quote, setQuote] = useState(quotes[0].quote);
+  const [author, setAuthor] = useState(quotes[0].author);
+  const [image, setImage] = useState(quotes[0].image);
 
-  randomQuote() {
+  const randomQuote = () => {
     const randomNumber = Math.floor(Math.random() * quotes.length);
     return quotes[randomNumber];
   }
-  shuffleQuotes(arr) {
+  const shuffleQuotes = (arr) => {
     return arr.sort(() => Math.random() - 0.5);
   }
-  handleClick = () => {
-    const generateQuote = this.randomQuote();
-    this.setState({
-      quote: generateQuote.quote,
-      author: generateQuote.author,
-      image: generateQuote.image,
-    });
-    this.shuffleQuotes(quotes);
+  const handleClick = () => {
+    const generateQuote = randomQuote();
+    setQuote(generateQuote.quote);
+    setAuthor(generateQuote.author);
+    setImage(generateQuote.image);
+    shuffleQuotes(quotes);
   };
 
-  render() {
-    return (
-      <div>
-        <h1 className="page-title">Simpsons Quote Generator!</h1>
+  return (
+    <div>
+      <h1 className="page-title">Simpsons Quote Generator!</h1>
 
-        <QuoteContainer handleClick={this.handleClick} {...this.state} />
-      </div>
-    );
-  }
+      <QuoteContainer handleClick={handleClick} quote={quote} author={author} image={image} />
+    </div>
+  );
 }
 
 export default Home;
